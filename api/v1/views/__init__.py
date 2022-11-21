17eada8e765703efaa91260f48dd43d5560be8e0
BOSTONE069 <bostoneochieng69@gmail.com>
2022-11-21 14:59:33 -0500

    updated the init file

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,10 +1,16 @@
+#!/usr/bin/python3
+"""
+this is the __init__.py file
+"""
 from flask import Blueprint
+
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-from api.v1.views.index import *  # noqa
-from api.v1.views.states import *  # noqa
-from api.v1.views.cities import *  # noqa
-from api.v1.views.amenities import *  # noqa
-from api.v1.views.users import *  # noqa
-from api.v1.views.places import *  # noqa
-from api.v1.views.places_reviews import *  # noqa
-from api.v1.views.places_amenities import *  # noqa
+
+from api.v1.views.index import *
+from api.v1.views import states
+from api.v1.views import cities
+from api.v1.views import amenities
+from api.v1.views import users
+from api.v1.views import places
+from api.v1.views import places_reviews
+from api.v1.views import places_amenities
